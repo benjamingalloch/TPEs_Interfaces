95ff0c79c7ab0fc594f844e0afde22ba260dc285
Benjamin Gallo <benjamin221999@gmail.com>
2023-10-09 12:10:24 -0300

    termino login form y acomodo animacion

--- TP2/EntregaFinal/js/index.js
+++ TP2/EntregaFinal/js/index.js
@@ -1,28 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     var estado = false;
-
-    document.querySelector('.alternative-btn').addEventListener('click', function() {
-        var portada = document.querySelector('.front-page-container');
-        var loginOptions = document.querySelector('.index-forms');
-        var registerOptions = document.querySelector('.register-optios');
-        
-        if (!estado) {
-            portada.style.transform = 'translateX(470px)';
-            loginOptions.style.transform = 'translateX(-600px)';
-
-            //loginOptions.classList.add('hidden');
-            //registerOptions.classList.remove('hidden');
-
-            estado = true;
-        } else {
-            portada.style.transform = 'translateX(0px)';
-            loginOptions.style.transform = 'translateX(0px)';
-
-            //loginOptions.classList.remove('hidden');
-            //registerOptions.classList.add('hidden');
-
-            estado = false;
-        }
+    var buttons = document.querySelectorAll('.alternative-btn');
+    
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var portada = document.querySelector('.front-page-container');
+            var loginOptions = document.querySelector('.index-forms');
+            var registerOptions = document.querySelector('.register-optios');
+            if (!estado) {
+                portada.style.transform = 'translateX(470px)';
+                loginOptions.style.transform = 'translateX(-600px)';
+                estado = true;
+            } else {
+                portada.style.transform = 'translateX(0px)';
+                loginOptions.style.transform = 'translateX(0px)';
+                estado = false;
+            }
+        });
     });
 });
+
 
