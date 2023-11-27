9ad011a86d30b21116f238e9bc0625989bae4218
Duroca <atilio6999@gmail.com>
2023-11-27 15:06:09 -0300

    agrego top y navbar

---
+++ TP4/EntregaFinal/js/burger_menu.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector('.burger');
+    const line1 = document.querySelector('.line1');
+    const line2 = document.querySelector('.line2');
+    const line3 = document.querySelector('.line3');
+
+    burger.addEventListener('click', () =>{
+        line1.classList.toggle('active');
+        line2.classList.toggle('active');
+        line3.classList.toggle('active');
+});
